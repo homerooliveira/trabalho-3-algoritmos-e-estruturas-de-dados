d54bbab0e7f360a7763f942c017441fec29c3326
Homero Junior <homero_junior_oliveira@outlook.com>
2016-11-19 21:41:27 -0200

    Adicionado classe PageOfBook para usar como modelo dos dados

---
+++ src/br/com/pucrs/PageOfBook.java
@@ -0,0 +1,27 @@
+package br.com.pucrs;
+
+public class PageOfBook {
+    private String content;
+    private String type;
+
+    public PageOfBook(String content, String type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
